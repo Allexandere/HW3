1b8293f8b1b9cf57fa4efb20ce2dea9dc51004c3
Allexandere <39259989+Allexandere@users.noreply.github.com>
2021-12-14 19:21:56 +0300

    init

---
+++ scripts/variables.js
@@ -0,0 +1,27 @@
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const num3 = document.getElementById("num3");
+const num4 = document.getElementById("num4");
+const num5 = document.getElementById("num5");
+const num6 = document.getElementById("num6");
+const num7 = document.getElementById("num7");
+const num8 = document.getElementById("num8");
+const num9 = document.getElementById("num9");
+const num0 = document.getElementById("num0");
+
+const div = document.getElementById("div");
+const mlt = document.getElementById("mlt");
+const min = document.getElementById("min");
+const dot = document.getElementById("dot");
+const eql = document.getElementById("eql");
+const pls = document.getElementById("pls");
+const clr = document.getElementById("clr");
+
+const displ = document.getElementById("display");
+
+const DIV_OP = "DIV_OP";
+const MLT_OP = "MLT_OP";
+const MIN_OP = "MIN_OP";
+const DOT_OP = "DOT_OP";
+const EQL_OP = "EQL_OP";
+const PLS_OP = "PLS_OP";
